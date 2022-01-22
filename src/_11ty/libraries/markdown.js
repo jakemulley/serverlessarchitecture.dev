7f012858fbe6d489e415f58c656ded65213f9d67
Jake Mulley <jakemulley@users.noreply.github.com>
2022-01-22 16:00:37 +0000

    Add a simple API example for reference architecture

--- src/_11ty/libraries/markdown.js
+++ src/_11ty/libraries/markdown.js
@@ -2,7 +2,11 @@ const slugify = require('slugify')
 const markdownIt = require('markdown-it')
 const markdownItAnchor = require('markdown-it-anchor')
 
-module.exports = markdownIt().use(markdownItAnchor, {
+module.exports = markdownIt({
+  html: true,
+  linkify: true,
+  typographer: true
+}).use(markdownItAnchor, {
   slugify (string) {
     return slugify(string, {
       lower: true
---
+++ src/reference-architecture/simple-api/index.md
@@ -0,0 +1,12 @@
+---
+title: Simple API
+tagline: Reference architecture for a simple serverless API.
+---
+
+## Generic
+
+{% asyncUml2Svg page.url + "generic.uml" %}
+
+## AWS
+
+{% asyncUml2Svg page.url + "aws.uml" %}
